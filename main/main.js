a12ac0ab3de3455ebbeb55008e944459f4245fe7
vivien <1210717511@qq.com>
2019-05-31 16:26:29 +0800

    my code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,24 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+
+	var dic = inputs;
+	var a = [];
+	for (var key in dic){
+        a.push(dic[key]);
+     }
+
+	var price;
+	if(a[0]<=2){
+		price = 6;	
+	}
+	else if(a[0]>2&&a[0]<=8){
+		price = 6 + (a[0]-2)*0.8;			
+	}
+	else if(a[0]>8){
+		price = 10.8 + (a[0]-8)*1.2;
+	}
+	
+	if(a[1]!=0)
+		price += a[1]*0.25;
+	
+    return Math.round(price);
 };
